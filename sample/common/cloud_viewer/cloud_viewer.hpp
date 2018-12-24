569381eb8f7c4aea9a2a89fc538c019eb708721a
vincent.chen <chentuoqian@percipio.xyz>
2018-12-24 10:56:26 +0800

    add cloud_viewer code in sample common
    Signed-off-by: vincent.chen <chentuoqian@percipio.xyz>

---
+++ sample/common/cloud_viewer/cloud_viewer.hpp
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////
+/// A simple opengl cloud viewer
+/// Copyright(C)2016-2018 Percipio All Rights Reserved
+///////////////////////////////////////////////
+#ifndef CLOUD_VIWER_HPP__
+#define CLOUD_VIWER_HPP__
+#include <TYApi.h>
+#include <vector>
+
+
+
+class GLPointCloudViewer{
+public:
+
+    static int GlInit();
+    static int EnterMainLoop();
+    static int Update(int point_num, const TY_VECT_3F* points, const uint8_t* color);
+    static int ResetViewTranslate();
+    static int RegisterKeyCallback(bool(*callback)(int));
+    static int Deinit();//destroy all & exit
+
+private:
+    GLPointCloudViewer(){}
+};
+
+
+
+
+#endif
