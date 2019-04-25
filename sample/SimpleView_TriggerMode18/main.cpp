448e1060d711eec173b1d7cb7f43753e1eea7bbe
vincent.chen <chentuoqian@percipio.xyz>
2019-04-25 14:21:41 +0800

    add two samples for part of percipio camera
    Signed-off-by: vincent.chen <chentuoqian@percipio.xyz>

---
+++ sample/SimpleView_TriggerMode18/main.cpp
@@ -0,0 +1,183 @@
+#include "../common/common.hpp"
+
+void eventCallback(TY_EVENT_INFO *event_info, void *userdata)
+{
+    if (event_info->eventId == TY_EVENT_DEVICE_OFFLINE) {
+        LOGD("=== Event Callback: Device Offline!");
+        // Note: 
+        //     Please set TY_BOOL_KEEP_ALIVE_ONOFF feature to false if you need to debug with breakpoint!
+    }
+    else if (event_info->eventId == TY_EVENT_LICENSE_ERROR) {
+        LOGD("=== Event Callback: License Error!");
+    }
+}
+
+//Notice: This sample is used for part of percipio camera
+
+int main(int argc, char* argv[])
+{
+    std::string ID, IP;
+    TY_INTERFACE_HANDLE hIface = NULL;
+    TY_DEV_HANDLE hDevice = NULL;
+    int32_t color, ir, depth;
+    color = ir = depth = 1;
+    int duty = 2;
+
+    for(int i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "-id") == 0){
+            ID = argv[++i];
+        } else if(strcmp(argv[i], "-ip") == 0) {
+            IP = argv[++i];
+        } else if(strcmp(argv[i], "-color=off") == 0) {
+            color = 0;
+        } else if(strcmp(argv[i], "-depth=off") == 0) {
+            depth = 0;
+        } else if(strcmp(argv[i], "-ir=off") == 0) {
+            ir = 0;
+        } else if(strcmp(argv[i], "-duty") == 0) {
+            duty = atoi(argv[++i]);
+        } else if(strcmp(argv[i], "-h") == 0) {
+            LOGI("Usage: SimpleView_Triggermode18 [-h]");
+            return 0;
+        }
+    }
+
+    if (!color && !depth && !ir) {
+        LOGD("=== At least one component need to be on");
+        return -1;
+    }
+
+    LOGD("=== Init lib");
+    ASSERT_OK( TYInitLib() );
+    TY_VERSION_INFO ver;
+    ASSERT_OK( TYLibVersion(&ver) );
+    LOGD("     - lib version: %d.%d.%d", ver.major, ver.minor, ver.patch);
+
+    std::vector<TY_DEVICE_BASE_INFO> selected;
+    ASSERT_OK( selectDevice(TY_INTERFACE_ALL, ID, IP, 1, selected) );
+    ASSERT(selected.size() > 0);
+    TY_DEVICE_BASE_INFO& selectedDev = selected[0];
+
+    ASSERT_OK( TYOpenInterface(selectedDev.iface.id, &hIface) );
+    ASSERT_OK( TYOpenDevice(hIface, selectedDev.id, &hDevice) );
+
+    int32_t allComps;
+    ASSERT_OK( TYGetComponentIDs(hDevice, &allComps) );
+    if(allComps & TY_COMPONENT_RGB_CAM  && color) {
+        LOGD("=== Has RGB camera, open RGB cam");
+        ASSERT_OK( TYEnableComponents(hDevice, TY_COMPONENT_RGB_CAM) );
+    }
+
+    int32_t componentIDs = 0;
+    LOGD("=== Configure components, open depth cam");
+    if (depth) {
+        componentIDs = TY_COMPONENT_DEPTH_CAM;
+    }
+
+    if (ir) {
+        componentIDs |= TY_COMPONENT_IR_CAM_LEFT;
+    }
+
+    ASSERT_OK( TYEnableComponents(hDevice, componentIDs) );
+
+    LOGD("=== Configure feature, set resolution to 640x480.");
+    int err = TYSetEnum(hDevice, TY_COMPONENT_DEPTH_CAM, TY_ENUM_IMAGE_MODE, TY_IMAGE_MODE_DEPTH16_640x480);
+    ASSERT(err == TY_STATUS_OK || err == TY_STATUS_NOT_PERMITTED);
+
+    LOGD("=== Prepare image buffer");
+    uint32_t frameSize;
+    ASSERT_OK( TYGetFrameBufferSize(hDevice, &frameSize) );
+    LOGD("     - Get size of framebuffer, %d", frameSize);
+
+    LOGD("     - Allocate & enqueue buffers");
+    char* frameBuffer[2];
+    frameBuffer[0] = new char[frameSize];
+    frameBuffer[1] = new char[frameSize];
+    LOGD("     - Enqueue buffer (%p, %d)", frameBuffer[0], frameSize);
+    ASSERT_OK( TYEnqueueBuffer(hDevice, frameBuffer[0], frameSize) );
+    LOGD("     - Enqueue buffer (%p, %d)", frameBuffer[1], frameSize);
+    ASSERT_OK( TYEnqueueBuffer(hDevice, frameBuffer[1], frameSize) );
+
+    LOGD("=== Register event callback");
+    ASSERT_OK(TYRegisterEventCallback(hDevice, eventCallback, NULL));
+
+    LOGD("=== Set trigger to trig mode 18");
+    TY_TRIGGER_PARAM trigger;
+    trigger.mode = TY_TRIGGER_MODE_SIG_PASS;
+    trigger.fps = 10;
+    trigger.rsvd = duty;
+    ASSERT_OK(TYSetStruct(hDevice, TY_COMPONENT_DEVICE, TY_STRUCT_TRIGGER_PARAM, &trigger, sizeof(trigger)));
+
+    LOGD("=== Enable Resend Option");
+    ASSERT_OK(TYSetBool(hDevice, TY_COMPONENT_DEVICE, TY_BOOL_GVSP_RESEND, true));
+
+    LOGD("=== Start capture");
+    ASSERT_OK( TYStartCapture(hDevice) );
+
+    LOGD("=== While loop to fetch frame");
+    bool exit_main = false;
+    TY_FRAME_DATA frame;
+    DepthViewer depthViewer("SimpleView_FetchFrame");
+    int index = 0;
+    int cnt = 0;
+
+    LOGD("=== Send Trigger Command");
+    ASSERT_OK(TYSendSoftTrigger(hDevice));
+
+    while(!exit_main) {
+        int err = TYFetchFrame(hDevice, &frame, 20000);
+        if (err == TY_STATUS_OK) {
+            LOGD("=== Get frame %d", ++index);
+            for (int32_t i=0; i<frame.validCount; i++) {
+                LOGD("    [%d]%4d*%4d (%d)", i,
+                    frame.image[i].width, frame.image[i].height, frame.image[i].reserved[0]); // laser_val: frame.image[i].reserved[0]
+            }
+
+            int fps = get_fps();
+            if (fps > 0) {
+                LOGI("fps: %d", fps);
+            }
+
+            cv::Mat depth, irl, irr, color;
+            parseFrame(frame, &depth, &irl, &irr, &color);
+            if (!depth.empty()) {
+                depthViewer.show(depth);
+            }
+            if (!irl.empty()) { cv::imshow("LeftIR", irl); }
+            if (!irr.empty()) { cv::imshow("RightIR", irr); }
+            if (!color.empty()) { cv::imshow("Color", color); }
+
+            int key = cv::waitKey(1);
+            switch (key & 0xff) {
+            case 0xff:
+                break;
+            case 'q':
+                exit_main = true;
+                break;
+            default:
+                LOGD("Unmapped key %d", key);
+            }
+
+            LOGD("=== Re-enqueue buffer(%p, %d)"
+                , frame.userBuffer, frame.bufferSize);
+            ASSERT_OK(TYEnqueueBuffer(hDevice, frame.userBuffer, frame.bufferSize));
+        }
+
+        cnt++;
+        if (cnt == duty + 1) {
+            LOGD("=== Send Trigger Command");
+            TYSendSoftTrigger(hDevice);
+            cnt = 0;
+        }
+    }
+
+    ASSERT_OK( TYStopCapture(hDevice) );
+    ASSERT_OK( TYCloseDevice(hDevice) );
+    ASSERT_OK( TYCloseInterface(hIface) );
+    ASSERT_OK( TYDeinitLib() );
+    delete frameBuffer[0];
+    delete frameBuffer[1];
+
+    LOGD("=== Main done!");
+    return 0;
+}
